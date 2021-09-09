19a1b0637243c114f8d1a9187c3c1dd941154c0d
Valerio Oz <valerio.oz@outlook.com>
2021-09-09 15:40:11 +0000

    Fix: testing with array syntax

--- bg.js
+++ bg.js
@@ -104,7 +104,7 @@ function createSubFolder(accessTime) {
 // close all old inactive and unpinned tabs 
 function garbageCollect() {
     // remove
-    let accessTimesArray = Object.entries(accessTimes).map((timestamp, index) => ({ id: index, timestamp: timestamp }))
+    let accessTimesArray = Object.entries(accessTimes).map((el) => ({ id: el[0], timestamp: el[1] }))
     accessTimesArray.forEach(accessTime => {
         var tabId = parseInt(accessTime.id, 10);
         var accessTime = accessTime.timestamp;
