100f74041093bf54f3ca46689a8fd383cd6cba5b
Valerio Oz <valerio.oz@outlook.com>
2021-09-09 18:21:20 +0000

    fix: archivemode checkbox state

--- options.js
+++ options.js
@@ -12,18 +12,18 @@ function restore_options() {
   var old_age_mins = localStorage["old_age_mins"];
   var archive_mode_value = localStorage["archive_mode"];
   if (!old_age_mins) {
-    old_age_mins =  60*4;
+    old_age_mins = 60 * 4;
   }
 
   var oldage = document.getElementById("oldage");
   oldage.value = old_age_mins;
   var archivemode = document.getElementById("archivemode");
-  archivemode.checked = archive_mode;
+  archivemode.checked = archivemode;
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#save').addEventListener('click',  save);
+  document.querySelector('#save').addEventListener('click', save);
 
   restore_options();
 });
